a07946fade9a8a86c9ee1c7437aaf6506bfc9ffd
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-25 23:48:47 +0530

    [arvind] Add: branch4 created

--- FunctionalProgramming/Functionalcode.cs
+++ FunctionalProgramming/Functionalcode.cs
@@ -6,43 +6,20 @@ using System.Threading.Tasks;
 
 namespace FunctionalProgramming
 {
-    internal class AlphVow
+    internal class EvenOdd
     {
-        char ch;
-        public void alfavow()
+        public void checkev()
         {
-            Console.Write("use input as  Alphabet (A-Z or a-z):");
-            ch = Convert.ToChar(Console.ReadLine().ToLower());
-            int i = ch;
-            if (i >= 48 && i <= 57)
+            Console.WriteLine("enter any number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
             {
-                Console.Write("Please enter an alpahbet number.");
+                Console.WriteLine("this is even number");
             }
             else
             {
-                switch (ch)
-                {
-                    case 'a':
-                        Console.WriteLine("The Alphabet is vowel");
-                        break;
-                    case 'e':
-                        Console.WriteLine("The Alphabet is vowel");
-                        break;
-                    case 'i':
-                        Console.WriteLine("The Alphabet is vowel");
-                        break;
-                    case 'o':
-                        Console.WriteLine("The Alphabet is vowel");
-                        break;
-                    case 'u':
-                        Console.WriteLine("The Alphabet is vowel");
-                        break;
-                    default:
-                        Console.WriteLine("The Alphabet is not a vowel");
-                        break;
-                }
+                Console.WriteLine("this is odd number");
             }
-            Console.ReadKey();
         }
     }
 }
