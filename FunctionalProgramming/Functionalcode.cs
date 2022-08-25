a7f97d3005bb191a02f6764fc26894fa31ff69e6
Arvind Gill <arvindkhtmuz@gmail.com>
2022-08-25 23:37:03 +0530

    [arvind] Add: folder structure created

---
+++ FunctionalProgramming/Functionalcode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    internal class LeapYear
+    {
+        public void yearcheck()
+        {
+            Console.WriteLine("enter any year:");
+            int year = int.Parse(Console.ReadLine());
+            if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine("leap year");
+            }
+            else
+            {
+                Console.WriteLine("not a leap year");
+            }
+        }
+    }
+}
